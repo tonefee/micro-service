77145c7c4487a4d4204c772fbaf790d64f113bd9
tangyifei <15062230055@163.com>
2019-06-18 18:17:30 +0800

    提交人：唐一菲 提交事由：修改包的路径 提交时间：2019年6月18日18:17:24

--- fukun-core/fukun-core-gateway/fukun-core-gateway-server/src/main/java/com/fukun/gateway/controller/HealthCheckController.java
+++ fukun-core/fukun-core-gateway/fukun-core-gateway-server/src/main/java/com/fukun/gateway/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.fukun.consul.consumer.controller;
+package com.fukun.gateway.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
