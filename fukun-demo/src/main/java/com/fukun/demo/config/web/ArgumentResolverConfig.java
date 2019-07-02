9a71984cbae467236145f1f9bc5331af2bd498e8
tangyifei <15062230055@163.com>
2019-07-02 20:03:35 +0800

    提交人：唐一菲 提交事由：替换过时的webMvc配置器 提交时间：2019年7月2日20:03:27

--- fukun-demo/src/main/java/com/fukun/demo/config/web/ArgumentResolverConfig.java
+++ fukun-demo/src/main/java/com/fukun/demo/config/web/ArgumentResolverConfig.java
@@ -3,7 +3,7 @@ package com.fukun.demo.config.web;
 import com.fukun.commons.web.resolver.LoginUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
@@ -14,7 +14,7 @@ import java.util.List;
  * @since 2019-5-24 14:46:35
  */
 @Configuration
-public class ArgumentResolverConfig extends WebMvcConfigurerAdapter {
+public class ArgumentResolverConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
