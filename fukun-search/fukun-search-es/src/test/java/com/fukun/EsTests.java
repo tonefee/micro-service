5cc2665e4f3039fcf0b52f7971dcf115e48c7a44
tangyifei <15062230055@163.com>
2019-07-27 15:07:57 +0800

    提交人：唐一菲 提交事由：增加es的单元测试类完成基本的es的crud操作 提交时间：2019年7月27日15:07:49

--- fukun-search/fukun-search-es/src/test/java/com/fukun/EsTests.java
+++ fukun-search/fukun-search-es/src/test/java/com/fukun/EsTests.java
@@ -7,6 +7,8 @@ import org.apache.http.HttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -109,18 +111,13 @@ public class EsTests {
 
                 .endObject()
 
-                .startObject("settings")
-                .field("number_of_shards", 3)
-                .field("number_of_replicas", 1)
-                .endObject()
-
                 .endObject();
         indexRequest.source(builder);
         // 生成json字符串
         String source = indexRequest.source().utf8ToString();
         HttpEntity entity = new NStringEntity(source, ContentType.APPLICATION_JSON);
         // 使用RestClient进行操作 而非rhlClient
-        Request request = new Request("PUT", "/news/_mapping");
+        Request request = new Request("PUT", "/" + INDEX + "/_mapping");
         request.setEntity(entity);
         Response response = restClient.performRequest(request);
         System.out.println(response);
@@ -152,12 +149,14 @@ public class EsTests {
      */
     @Test
     public void addTest() throws Exception {
+        String id = "6";
         IndexRequest indexRequest = new IndexRequest(INDEX);
+        indexRequest.id(id);
         News news = new News();
-        news.setId("1");
-        news.setTitle("中国产小型无人机的“对手”来了，俄微型拦截导弹便宜量又多");
-        news.setTag("军事");
-        news.setPublishTime("2018-01-24T23:59:30Z");
+        news.setId(id);
+        news.setTitle("江苏南京进入高温模式，局部气温36度");
+        news.setTag("气象");
+        news.setPublishTime("2019-07-27T23:59:30Z");
         String source = JsonUtil.object2Json(news);
         indexRequest.source(source, XContentType.JSON);
         rhlClient.index(indexRequest, RequestOptions.DEFAULT);
@@ -186,8 +185,8 @@ public class EsTests {
     @Test
     public void updateTest() throws Exception {
         UpdateRequest updateRequest = new UpdateRequest(INDEX, "1");
-        Map<String, String> map = new HashMap<>(1 << 1);
-        map.put("tag", "网球");
+        Map<String, Object> map = new HashMap<>(1 << 1);
+        map.put("tag", "军事论坛");
         updateRequest.doc(map);
         rhlClient.update(updateRequest, RequestOptions.DEFAULT);
     }
@@ -220,6 +219,91 @@ public class EsTests {
         System.out.println(response);
     }
 
+    /**
+     * 根据文档的id删除文档
+     *
+     * @throws Exception 可能抛出的异常
+     */
+    @Test
+    public void delete() throws Exception {
+        DeleteRequest deleteRequest = new DeleteRequest(INDEX, "1");
+        DeleteResponse response = rhlClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        System.out.println(response);
+    }
+
+    /**
+     * 删除title包含费德勒的新闻
+     * 使用 RestHighLevelClient 根据查询条件删除文档，先查询出满足条件的文档，再根据文档ID依次删除，即发送了两次请求。
+     * 就删除操作而言，RestHighLevelClient所能做的还不够完善，因此要联系RestClient的灵活性才能实现我们想要的功能。
+     *
+     * @throws Exception 可能抛出的异常
+     */
+//    @Test
+//    public void deleteByQueryWithRestHighLevelClient() throws Exception {
+//        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+//        sourceBuilder.timeout(new TimeValue(2, TimeUnit.SECONDS));
+//        TermQueryBuilder termQueryBuilder1 = QueryBuilders.termQuery("title.keyword", "费德勒");
+//        sourceBuilder.query(termQueryBuilder1);
+//        SearchRequest searchRequest = new SearchRequest(INDEX);
+//        searchRequest.source(sourceBuilder);
+//        SearchResponse response = rhlClient.search(searchRequest, RequestOptions.DEFAULT);
+//        SearchHits hits = response.getHits();
+//        // 先根据条件查询出符合查询条件的文档id
+//        List<String> docIds = new ArrayList<>(hits.getHits().length);
+//        for(SearchHit hit : hits) {
+//            docIds.add(hit.getId());
+//        }
+//
+//        BulkRequest bulkRequest = new BulkRequest();
+//        // 再根据查询条件查询出的文档id来删除文档
+//        for(String id : docIds) {
+//            DeleteRequest deleteRequest = new DeleteRequest(INDEX, id);
+//            bulkRequest.add(deleteRequest);
+//        }
+//        rhlClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+//    }
+
+    /**
+     * 删除 tag 包含体育的新闻
+     * 使用 RestClient 直接以http的形式调用ES服务执行根据条件执行删除操作。
+     * 优点就在于不需要发起两次HTTP连接，节省时间
+     *
+     * @throws Exception 可能抛出的异常
+     */
+    @Test
+    public void deleteByQueryWithRestClient() throws Exception {
+        String endPoint = "/" + INDEX + "/_delete_by_query";
+        String source = generateQueryString();
+        HttpEntity entity = new NStringEntity(source, ContentType.APPLICATION_JSON);
+        Request request = new Request("POST", endPoint);
+        request.setEntity(entity);
+        restClient.performRequest(request);
+    }
+
+    private String generateQueryString() throws Exception {
+        IndexRequest indexRequest = new IndexRequest();
+        XContentBuilder builder;
+        builder = JsonXContent.contentBuilder()
+                .startObject()
+                  .startObject("query")
+                     .startObject("bool")
+                            .startArray("must")
+                              .startObject()
+                                .startObject("term")
+                                    .startObject("tag")
+                                    .field("value", "体育")
+                                    .endObject()
+                                .endObject()
+                              .endObject()
+                            .endArray()
+                     .endObject()
+                  .endObject()
+                .endObject();
+        indexRequest.source(builder);
+        String source = indexRequest.source().utf8ToString();
+        return source;
+    }
+
     private List<IndexRequest> generateRequests() {
         List<IndexRequest> requests = new ArrayList<>(1 << 2);
         requests.add(generateNewsRequest("1", "中印边防军于拉达克举行会晤 强调维护边境和平", "军事", "2018-01-27T08:34:00Z"));
@@ -231,6 +315,7 @@ public class EsTests {
 
     private IndexRequest generateNewsRequest(String id, String title, String tag, String publishTime) {
         IndexRequest indexRequest = new IndexRequest(INDEX);
+        indexRequest.id(id);
         News news = new News();
         news.setId(id);
         news.setTitle(title);
