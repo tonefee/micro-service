e7b62fe0d11c1443ea86d78c8e66597f07f25dee
tangyifei <15062230055@163.com>
2019-08-20 17:46:25 +0800

    提交人：唐一菲 提交事由：增加获取bean的相关方法 提交时间：2019年8月20日17:46:19

--- fukun-commons/fukun-commons-util/src/main/java/com/fukun/commons/util/RequestContextUtil.java
+++ fukun-commons/fukun-commons-util/src/main/java/com/fukun/commons/util/RequestContextUtil.java
@@ -65,8 +65,39 @@ public class RequestContextUtil implements ApplicationContextAware {
         RequestContextUtil.applicationContext = applicationContext;
     }
 
+    /**
+     * 取得存储在静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        checkApplicationContext();
+        return applicationContext;
+    }
+
     public static <T> T getBean(String name) {
+        checkApplicationContext();
         return (T) applicationContext.getBean(name);
+    }
+
+    /**
+     * 从静态变量ApplicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<T> clazz) {
+        checkApplicationContext();
+        return (T) applicationContext.getBeansOfType(clazz);
+    }
+
+    /**
+     * 清除applicationContext静态变量.
+     */
+    public static void cleanApplicationContext() {
+        applicationContext = null;
+    }
+
+    private static void checkApplicationContext() {
+        if (applicationContext == null) {
+            throw new IllegalStateException("applicaitonContext未注入,请在applicationContext.xml中定义SpringContextHolder");
+        }
     }
 
 }
